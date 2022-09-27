9e9a2f08bfeb158dde4ab8924f0118253653c137
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-09-26 22:18:00 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ var game = "notselected";
 
 var windowResizeX = true, windowResizeY = false;
 
-var version = 1.22293, mostRecentVersion = null, reloadButton, 
+var version = 1.22294, mostRecentVersion = null, reloadButton, 
 LatestUpdatePlatformsAimed = ""/* PC, Mobile, Android, iPhone, iPad, iPhoneXR, All */;//1.22293
 
 var infiniteflightbutton, infiniteracebutton;
